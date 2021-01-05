17241545e1a7afa8c180e3edca6088c92b96431f
Paulo Madson Da Silva <paulomadson16@gmail.com>
2021-01-04 23:39:35 -0300

    Project created

---
+++ backend/src/test/java/com/devsuperior/PMDelivery/PmDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.PMDelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
