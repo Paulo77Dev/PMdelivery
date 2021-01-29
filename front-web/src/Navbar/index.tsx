ef0a13e55f5883fc9c41b6adb7e7db9b8b6e9e79
Paulo Madson Da Silva <paulomadson16@gmail.com>
2021-01-29 04:45:43 -0300

    test

--- front-web/src/Navbar/index.tsx
+++ front-web/src/Navbar/index.tsx
@@ -1,5 +1,5 @@
 import './styles.css';
-import {ReactComponent as Logo} from './logo.svg'
+import {ReactComponent as Logo} from './ptlogo.svg'
 import { Link } from 'react-router-dom';
 
 function Navbar(){
